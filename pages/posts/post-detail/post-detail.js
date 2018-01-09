a48f93db3dbc9a19481c7632ba45cca35acc78ca
nick197504 <79252568@qq.com>
2018-01-09 14:20:34 +0800

    changed code

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,21 +1,10 @@
-var postListData = require("../../data/posts-data")
-
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
-  },
-
-  onTap: function(event){
-    var postid = event.currentTarget.dataset.postid;
-    console.log("onTap");
-    console.log("post id is"+ postid);
-    wx.navigateTo({      
-      url: 'post-detail/post-detail',
-    })
+    
   },
 
   /**
@@ -23,55 +12,54 @@ Page({
    */
   onLoad: function (options) {
     
-    this.setData({ postList: postListData.postList});
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    
   }
 })
